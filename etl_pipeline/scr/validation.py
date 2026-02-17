5703091ddc3665a3c6e4f0e642e3d643a757923f
Luis Fernando <luisfernandowubi@gmail.com>
2026-02-17 09:47:31 -0300

    feat(validation): cria função para verificar colunas do df
    - Verifica se todas existem.
    
    - Verifica se a ordem das colunas está correta.

--- etl_pipeline/scr/validation.py
+++ etl_pipeline/scr/validation.py
@@ -13,6 +13,48 @@ INPUT_DIR = PROCESSED_DIR / 'renewable_energy_data_clean.csv'
 OUTPUT_PATH = PROCESSED_DIR / 'renewable_energy_data_validated.csv'
 
 
+def validate_columns(df):
+
+    logger.info("Iniciando validação de colunas...")
+
+    expected_columns[
+        'region',
+        'sub_region',
+        'country',
+        'iso3_code,m49_code',
+        'renewable_or_not',
+        'group_technology',
+        'technology',
+        'sub_technology',
+        'producer_type',
+        'year',
+        'eletricity_generation_gwh',
+        'eletricity_installed_capacity_mw',
+        'heat_generation_tj',
+        'total_public_flows_usd_m',
+        'international_public_flows_usd_m',
+        'capacity_per_capta_w'
+    ]
+
+    currently_columns = df.columns.tolist()
+
+    if expected_columns == currently_columns:
+        logger.info(f"Todas as {len(expected_columns)} colunas presentes e na ordem correta\n")
+    else:
+        missing = set(expected_columns) - set(currently_columns)
+        extra = set(currently_columns) - set(expected_columns)
+        wrong_order = currently_columns != expected_columns 
+
+    if missing:
+        logger.error(f"❌ Colunas faltando: {missing}")
+    if extra:
+        logger.error(f"❌ Colunas extras: {extra}")
+    if wrong_order:
+        logger.error(f"❌ Ordem incorreta!\n Esperando: {expected_columns}\n Atual: {currently_columns} ")   
+
+    return df                  
+
+
 
 
 
@@ -41,7 +83,7 @@ if __name__ == '_main__':
 
     df = pd.read_csv(INPUT_DIR)
     logger.info(f"📊 Carregados {df(len)} registros")
-    
+
 
     df = validate_columns(df)
 
